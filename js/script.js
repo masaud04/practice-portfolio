fa00f28a3bb783c760d7107f739a794c053403bc
masaud04 <ahmedmasaud942@gmail.com>
2024-08-18 22:32:24 +0600

    update

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// import { Application } from '@splinetool/runtime';
+
+// const canvas = document.querySelector('.img');
+// const app = new Application(canvas);
+// app.load('https://prod.spline.design/12Tn4MHZ1Dl7qFJi/scene.splinecode');
+
+
+
+let navBtn = document.getElementById("navBtn")
+
+navBtn.addEventListener("click", () => {
+
+    window.location.href = 'https://www.google.com/'
+    
+    // console.log('hello');
+    
+})
